63d623f09dd76a05028a9a47b19a805c59c486ac
lachlan <lachlan.deer@uzh.ch>
2018-04-02 23:19:31 +0200

    document lothiantaylor data, update variable names

---
+++ R/lothiantaylor.R
@@ -0,0 +1,22 @@
+#' lothiantaylor
+#'
+#' Hayashi Source: Lothian, J., and M. Taylor, 1996, "Real Exchange Rate Behavior: The Recent Float from the Perspective of the Past Two Centuries," Journal of Political Economy, 104,488-509.
+#'
+#' A time series data set at annual frequency of the US dollar / UK Sterling exchange rate and annual wholesale price indices.
+#'
+#' @section Notes: Used in Chapter 9.6 and Empirical Exercise 1 of Chapter 2.
+#'
+#' @docType data
+#'
+#' @usage data('lothiantaylor')
+#'
+#' @format A data.frame with 200 observations on 4 variables:
+#' \itemize{
+#'  \item \strong{year:} year
+#'  \item \strong{usd_gbp:} annual average US Dollar / UK Sterling exchange rate
+#'  \item \strong{wpi_us:} annual average Wholesale Price Index for the USA, with 1914 normalized to 100
+#'  \item \strong{wpi_uk:} annual average Wholesale Price Index for the UK, with 1914 normalized to 100
+#' }
+#' @source \url{https://sites.google.com/site/fumiohayashi/hayashi-econometrics/data-for-empirical}
+#' @examples  str(lothiantaylor)
+"lothiantaylor"
--- data-raw/lothiantaylor.R
+++ data-raw/lothiantaylor.R
@@ -1,8 +1,13 @@
 # process lt.xls data
 
 library(readxl)
+library(dplyr)
 
-lothiantaylor <- read_excel("data-raw/lt.xls")
+lothiantaylor <- read_excel("data-raw/lt.xls") %>%
+                    rename(year    = YEAR,
+                           usd_gbp = S,
+                           wpi_us  = USWPI,
+                           wpi_uk  = UKWPI)
 
 readr::write_csv(lothiantaylor, "data-raw/lothiantaylor.csv")
 devtools::use_data(lothiantaylor, overwrite = TRUE, compress = 'xz')
