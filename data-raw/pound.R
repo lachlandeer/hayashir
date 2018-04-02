710596100ecb347469f4536c30b60e9e322e6779
lachlan <lachlan.deer@uzh.ch>
2018-04-03 00:31:07 +0200

    update variable names for yen and pound data

--- data-raw/pound.R
+++ data-raw/pound.R
@@ -2,7 +2,13 @@
 
 library(readxl)
 
-pound <- read_excel("data-raw/pound.xls")
+pound <- read_excel("data-raw/pound.xls") %>%
+            rename(date = DATE,
+                   spot_rate = S,
+                   forward_30 = "F",
+                   spot_30 = S30
+            ) %>%
+            mutate(date = lubridate::ymd(date))
 
 readr::write_csv(pound, "data-raw/pound.csv")
 devtools::use_data(pound, overwrite = TRUE, compress = 'xz')
--- data-raw/yen.R
+++ data-raw/yen.R
@@ -2,7 +2,13 @@
 
 library(readxl)
 
-yen <- read_excel("data-raw/yen.xls")
+yen <- read_excel("data-raw/yen.xls") %>%
+            rename(date = DATE,
+                   spot_rate = S,
+                   forward_30 = "F",
+                   spot_30 = S30
+            ) %>%
+            mutate(date = lubridate::ymd(date))
 
 readr::write_csv(yen, "data-raw/yen.csv")
 devtools::use_data(yen, overwrite = TRUE, compress = 'xz')
