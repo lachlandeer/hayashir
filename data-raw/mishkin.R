3f3a3460db3088c6edd7c4306b762674e0d631a5
lachlan <lachlan.deer@uzh.ch>
2018-04-02 18:57:00 +0200

    correct data saved in mishkin script

--- data-raw/mishkin.R
+++ data-raw/mishkin.R
@@ -5,4 +5,4 @@ library(readxl)
 mishkin <- read_excel("data-raw/mishkin.xls")
 
 readr::write_csv(mishkin, "data-raw/mishkin.csv")
-devtools::use_data(nerlove, overwrite = TRUE, compress = 'xz')
+devtools::use_data(mishkin, overwrite = TRUE, compress = 'xz')
