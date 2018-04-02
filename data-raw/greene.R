c3fedb0a42e5ac19ebdc3f248f682958385a70ae
lachlan <lachlan.deer@uzh.ch>
2018-04-02 22:39:14 +0200

    update greene data with variable names that conform to documentation

--- data-raw/greene.R
+++ data-raw/greene.R
@@ -1,8 +1,20 @@
 # process greene.xls data
 
 library(readxl)
+library(dplyr)
 
-greene <- read_excel("data-raw/greene.xls")
+greene <- read_excel("data-raw/greene.xls") %>%
+            rename(firm_id       = ID,
+                   total_cost    = TC,
+                   output        = Q,
+                   price_labor   = PL,
+                   price_fuel    = PF,
+                   price_capital = PK,
+                   labor_share   = LABOR,
+                   capital_share = CAPITAL
+                ) %>%
+            mutate(fuel_share = 1 - capital_share - labor_share)
+
 
 readr::write_csv(greene, "data-raw/greene.csv")
 devtools::use_data(greene, overwrite = TRUE, compress = 'xz')
