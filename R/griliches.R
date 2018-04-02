d034c13d9e64209080bf0f6a3c9ad038d7aef4a0
lachlan <lachlan.deer@uzh.ch>
2018-04-03 00:04:10 +0200

    document griliches data and update variable names in data set

---
+++ R/griliches.R
@@ -0,0 +1,38 @@
+#' griliches
+#'
+#' Hayashi Source: Blackburn, M., and D. Neumark, 1992, "Unobserved Ability, Efficiency Wages, and Interindustry Wage Differentials," Quarterly Journal of Economics, 107, 142 1-1436.
+#'
+#' A panel data extract of the Young Men's Cohort of the National Longitudinal Survey (NLS-Y).
+#'
+#' @section Notes: Used in Chapter 3.9 and the Empirical Exercise of Chapter 3. Data is the Blackburn - Neumark sample after eliminating data with missing mother's education.
+#'
+#' @docType data
+#'
+#' @usage data('griliches')
+#'
+#' @format A data.frame with 758 observations on 20 variables:
+#' \itemize{
+#' \item \strong{southern_residence:} = 1 if individual lives in southern state when first interviewed
+#' \item \strong{southern_residence_80:} = 1 individual lives in south in 1980
+#' \item \strong{married:} = 1 if individual is married when first interviewed
+#' \item \strong{married_80:} = 1 if individual is married when interviewed in 1980
+#' \item \strong{lives_metro:} = 1 if individual lives in metropolitan area when first interviewed
+#' \item \strong{lives_metro_80:} = 1 if individual lives in metropolitan area in 1980
+#' \item \strong{mothers_educ:} mother's education measured in years
+#' \item \strong{iq_score:} score on the IQ test
+#' \item \strong{kww_score:} score on "Knowlegde of the World of Work" test
+#' \item \strong{year:} year was first interviewed
+#' \item \strong{age:} age of individual when first interviewed
+#' \item \strong{age_80:} age of individual in 1980
+#' \item \strong{education:} years of completed schooling when first interviewed
+#' \item \strong{education_80:} years of completed schooling in 1980
+#' \item \strong{experience:} years of work experience when first interviewed
+#' \item \strong{experience_80:} years of work experience in 1980
+#' \item \strong{tenure:} years of tenure when first interviewed
+#' \item \strong{tenure_80:} years of tenure in 1980
+#' \item \strong{log_wage:} log wage when first interviewd
+#' \item \strong{log_wage_80:} log wage in 1980
+#' }
+#' @source \url{https://sites.google.com/site/fumiohayashi/hayashi-econometrics/data-for-empirical}
+#' @examples  str(griliches)
+"griliches"
--- data-raw/griliches.R
+++ data-raw/griliches.R
@@ -1,8 +1,29 @@
 # process grilic.xls data
 
 library(readxl)
+library(dplyr)
 
-griliches <- read_excel("data-raw/grilic.xls")
+griliches <- read_excel("data-raw/grilic.xls") %>%
+                rename(southern_residence = RNS,
+                       southern_residence_80 = RNS80,
+                       married = MRT,
+                       married_80 = MRT80,
+                       lives_metro = SMSA,
+                       lives_metro_80 = SMSA80,
+                       mothers_educ = MED,
+                       iq_score = IQ,
+                       kww_score = KWW,
+                       year = YEAR,
+                       age = AGE,
+                       age_80 = AGE80,
+                       education = S,
+                       education_80 = S80,
+                       experience = EXPR,
+                       experience_80 = EXPR80,
+                       tenure = TENURE,
+                       tenure_80 = TENURE80,
+                       log_wage = LW,
+                       log_wage_80 = LW80)
 
 readr::write_csv(griliches, "data-raw/griliches.csv")
 devtools::use_data(griliches, overwrite = TRUE, compress = 'xz')
