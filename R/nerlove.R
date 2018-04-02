c4f3c1b93f57bd49feb201d5cef59e772ca7340a
lachlan <lachlan.deer@uzh.ch>
2018-04-02 22:23:49 +0200

    document nerlove data, rename variables to conform to documentation

---
+++ R/nerlove.R
@@ -0,0 +1,23 @@
+#' nerlove
+#'
+#' Hayashi Source: Nerlove, M., 1963, “Returns to Scale in Electricity Supply,” in C. Christ (ed.), Measurement in Economics: Studies in Mathematical Economics and Econometrics in Memory of Yehuda Grunfeld, Stanford: Stanford University Press.
+#'
+#' A cross-sectional data set on 145 firms in 44 states in the year 1955 with data on total costs, factor prices (the wage rate, the price of fuel, and the rental price of capital), and output of power plants.
+#'
+#' @section Notes: Used in Section 7 of Chapter 1 and the Empirical Exercise of Chapter 1.
+#'
+#' @docType data
+#'
+#' @usage data('nerlove')
+#'
+#' @format A data.frame with 145 observations on 5 variables:
+#' \itemize{
+#'  \item \strong{total_cost:} total cost of supply in millions of dollars
+#'  \item \strong{output:} in billions of kilowatt hours
+#'  \item \strong{labor_price:} the price of labor
+#'  \item \strong{fuel_price:} the price of fuels
+#'  \item \strong{capital_price:} the price of capital
+#' }
+#' @source \url{https://sites.google.com/site/fumiohayashi/hayashi-econometrics/data-for-empirical}
+#' @examples  str(nerlove)
+"nerlove"
--- data-raw/nerlove.R
+++ data-raw/nerlove.R
@@ -1,8 +1,14 @@
 # process nerlove.xls data
 
 library(readxl)
+library(dplyr)
 
-nerlove <- read_excel("data-raw/nerlove.xls")
+nerlove <- read_excel("data-raw/nerlove.xls") %>%
+             rename(total_cost    = TC,
+                    output        = Q,
+                    price_labor   = PL,
+                    price_fuel    = PF,
+                    price_capital = PK)
 
 readr::write_csv(nerlove, "data-raw/nerlove.csv")
 devtools::use_data(nerlove, overwrite = TRUE, compress = 'xz')
