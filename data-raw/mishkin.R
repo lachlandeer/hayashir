56d2ae3acbf4ccf0d3a8aa1fd2de6b679f4461f2
lachlan <lachlan.deer@uzh.ch>
2018-04-02 18:55:23 +0200

    add mishkin data

---
+++ data-raw/mishkin.R
@@ -0,0 +1,8 @@
+# process mishkin.xls data
+
+library(readxl)
+
+mishkin <- read_excel("data-raw/mishkin.xls")
+
+readr::write_csv(mishkin, "data-raw/mishkin.csv")
+devtools::use_data(nerlove, overwrite = TRUE, compress = 'xz')
