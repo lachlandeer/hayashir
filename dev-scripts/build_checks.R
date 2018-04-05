6939deaa232d41eeb6271651c5738919cded33c4
lachlan <lachlan.deer@uzh.ch>
2018-04-06 00:40:04 +0200

    check build on different OS using rhub

---
+++ dev-scripts/build_checks.R
@@ -0,0 +1,19 @@
+# Setup Continuous Integration checks
+devtools::use_travis()
+devtools::use_appveyor()
+
+# R-hub Build
+#rhub::list_validated_emails()
+#rhub::validate_email()
+
+#
+rhub::check_on_linux()
+rhub::check_on_windows()
+rhub::check_on_macos()
+rhub::check_on_debian()
+rhub::check_on_ubuntu()
+rhub::check_on_fedora()
+rhub::check_on_centos()
+
+rhub::check_with_valgrind()
+rhub::check_with_sanitizers()
